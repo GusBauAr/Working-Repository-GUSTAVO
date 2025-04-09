1a15a89349084a7b431aeebd51064f1b46e5b53b
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-09 09:22:52 -0400

    Add files, users/slice.ts,store/index.ts, and configuration ListOfUsers

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -1,50 +1,52 @@
-import {Card, Table,TableHead,TableRow,TableHeaderCell,TableBody,TableCell} from "@tremor/react"
+import {Badge,Card,Table,TableBody,TableCell,TableHead,TableHeaderCell,TableRow,Title} from '@tremor/react'
 
 
-const users:{
-    id:string;
-    name: string;
-    email: string;
-    github :string;
-
-}[]=[ 
-    {
-    id:"1",
-    name:"gustavo",
-    email:"gustavo@gmail.com",
-    github:"gusbauar"
-    },
-    
-
-
-];
 export function ListOfUsers () {
   
-    return (
-      <Card>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeaderCell> Id </TableHeaderCell>
-              <TableHeaderCell> Nombre </TableHeaderCell>
-              <TableHeaderCell> Email </TableHeaderCell>
-              <TableHeaderCell> Acciones </TableHeaderCell>
+
+  return (
+    <Card>
+      <Title>
+        Usuarios
+        <Badge></Badge>
+      </Title>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell> Id </TableHeaderCell>
+            <TableHeaderCell> Nombre </TableHeaderCell>
+            <TableHeaderCell> Email </TableHeaderCell>
+            <TableHeaderCell> Acciones </TableHeaderCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {users.map((item) => (
+            <TableRow key={item.name}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  style={{
+									  width: '32px',
+									  height: '32px',
+									  borderRadius: '50%',
+									  marginRight: '8px'
+                  }}
+                  src={`https://unavatar.io/github/${item.github}`}
+                  alt={item.name}
+                />
+                {item.name}
+              </TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>
+                <button type='button'>
+                 
+                </button>
+              </TableCell>
             </TableRow>
-          </TableHead>
-  
-          <TableBody>
-            {users.map((item) => (
-              <TableRow key={item.name}>
-                <TableCell>{item.id}</TableCell>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.email}</TableCell>
-                <TableCell>
-                  Acciones
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Card>
-    );
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  )
 }
