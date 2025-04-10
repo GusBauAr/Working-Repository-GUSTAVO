82071db89cec043d19be620e7b10bdc787a264ba
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-09 21:58:09 -0400

    configuration of each fileo

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-import { Provider } from 'react-redux'
-import { store } from './store'
-
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
