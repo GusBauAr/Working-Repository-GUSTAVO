2fb864dde9f265945fc27234f80734c594cd6382
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-11 13:32:08 -0400

    configuration, interface CRUD with tremor and Tailwind

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -4,3 +4,4 @@ import type { AppDispatch, RootState } from "../store";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
