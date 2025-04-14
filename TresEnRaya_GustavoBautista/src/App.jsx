650caac70a7c04c5a3a905d266b922294c6b020a
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-14 13:09:52 -0400

    component creation and estado,conditional rendering of the turn

--- TresEnRaya_GustavoBautista/src/App.jsx
+++ TresEnRaya_GustavoBautista/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
+
 const TURNS={
   X:'x',
   O:'o'
   }
   
-  const board = Array(9).fill(null)
+  
 
-  const Square = ({children, updateBoard, index}) => {
-    return(
-      <div className="square">
+  const Square = ({children, isSelected, updateBoard, index}) => {
+    
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+    // const className = `square ${isSelected ? 'is-selected' : ''}`
+  
+    return (
+      <div className={className}>
         {children}
 
       </div>
@@ -16,6 +22,15 @@ const TURNS={
 
 
   function App() {
+    const [board, setBoard] = useState(
+      Array(9).fill(null)
+    )
+    const [turn, setTurn] = useState(TURNS.X)
+
+    
+
+
+
     return (
       <main className="board">
         <h1> tres raya</h1>
@@ -28,14 +43,23 @@ const TURNS={
                   key={index}
                   index={index}
                 >
-                {index}
+                  {board[index]}
 
                 </Square>                
                 )
               })
             }
-            </section>
-            </main>
+        </section>
+        <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+        {TURNS.X}
+        </Square>
+
+        <Square isSelected={turn === TURNS.O}>
+        {TURNS.O}
+        </Square>
+        </section>
+      </main>
     )
   }           
     
