fe4a508f1d76b5d3b03564f5597fa43c91c3deca
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-17 18:47:07 -0400

    Arreglos

--- Course_JavaScript_GustavoBautista/01-fundamentos/assets/js/palabras-reservadas.js
+++ Course_JavaScript_GustavoBautista/01-fundamentos/assets/js/palabras-reservadas.js
@@ -9,11 +9,11 @@ let constante =123;
 //for
 //enum
 
-let 1abc = 123; //error
-let objeto$ = 123;
-let objeto123 = 123;
-let object.123 = 123; //error
-let precio99_99= 123;
+// let 1abc = 123; //error
+// let objeto$ = 123;
+// let objeto123 = 123;
+// let object.123 = 123; //error
+// let precio99_99= 123;
 
 //no es significativo
 let x ='gustavo';
