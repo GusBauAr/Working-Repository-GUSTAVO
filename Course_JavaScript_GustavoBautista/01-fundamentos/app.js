27b888b985e00f998d61dff4026bd7df09f7c5a1
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-17 11:21:58 -0400

    depuration and breakpoints

---
+++ Course_JavaScript_GustavoBautista/01-fundamentos/app.js
@@ -0,0 +1,30 @@
+
+//depuracion y breckpoints
+//lo vimos en inspeccionar Un breakpoint es un punto específico en el código donde le indicas al depurador (debugger) que detenga la ejecución del programa.
+let a = 10,
+    b = 20,
+    c = 'hola ',
+    d = 'spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// let a = 10;
+// let b = 10;
+// let c = 'hola';
+// let d = 'spiderman';
+// let x = a + b;
+
+c= 'hola de nuevo';
+
+
+// console.log(x);
+// console.log({a});
+// console.log({b});
+// console.log({c});
+// console.log({d});
+
+
+//console.table ({a, b, c, d})
+
+
