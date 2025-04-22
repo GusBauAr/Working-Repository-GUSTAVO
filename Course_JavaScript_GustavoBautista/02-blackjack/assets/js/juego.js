6b71a613a40a6668fec4f6b53e431e323dfbd3b9
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-21 21:14:24 -0400

    Pedir carta

--- Course_JavaScript_GustavoBautista/02-blackjack/assets/js/juego.js
+++ Course_JavaScript_GustavoBautista/02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos =['C', 'D', 'H', 'S'];
 const especiales =['A', 'J', 'Q', 'K'];
 
+//esta funcion crea un anueva baraja de cartas 
 const crearDeck = ( ) => {
     for (let i = 2; i <= 10; i++){
         for (let tipo of tipos){
@@ -20,10 +21,25 @@ const crearDeck = ( ) => {
             deck.push(esp + tipo);
         }
     }
-    console.log(deck);
+    
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 
 }
 crearDeck();
+
+//esta funcion me permite tomar una carta 
+const pedirCarta = () =>{
+    if (deck.length === 0){
+        throw 'no hay cartas en el deck';
+
+    }
+    
+    const carta = deck.pop();// selecciona la ultima posicion del array
+
+    console.log(deck);
+    console.log (carta);
+    return carta;
+}
+pedirCarta();
