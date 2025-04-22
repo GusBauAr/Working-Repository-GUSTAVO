9875fa6f74532fc6e4e327e79046519e044e96fd
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-22 18:19:25 -0400

    Sets y Gets

--- Course_JavaScript_GustavoBautista/01-fundamentos_v1.0.0/assets/js/classes/class.js
+++ Course_JavaScript_GustavoBautista/01-fundamentos_v1.0.0/assets/js/classes/class.js
@@ -3,10 +3,12 @@
 //el objetivo de clases es que podamos tener propiedades y metodos que sean comunes en todas las instancias de esta misma persona
 class Persona {
 
-    // se esta inicializando propiedades de clases
+    //  se esta inicializando propiedades de clases
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
+
 
     constructor (nombre = 'sin nombre', codigo = 'sin codigo', frase = 'sin frase'){//el constructor es un metodo que se va ejecutar exactamente en el momento que se crea una nueva instancia de Persona
                     //el contructor es el unico metodo que retorna una instancia de un objeto y no retorna underfined
@@ -14,6 +16,17 @@ class Persona {
         this.codigo = codigo;
         this.frase = frase;
     }
+    // controlar una propiedad, ejm: comida siempre tiende que estar en mayuscula
+    //EL SET ES PARA ESTABLECER UN VALOR
+    set setComidaFavorita(comida){//argunemnto (comida)
+        this.comida = comida.toUpperCase(); //para que salga en mayuscula todo lo que se establece con comida.
+    }
+    //EL GET ES PARA RECUPERAR UN VALOR
+    get getComidaFavorita(){ //vamos a recuperar una propiedad 
+        return `la comida favorita de ${this.nombre} es ${this.comida}`;
+
+    }
+
     //..................................................................................................
     //el metodo siempre se acostumbra que este despues del contructor
     //creamos un metodo si esta vacio el () quiere decir que no tiene argumentos y saldra undefined
@@ -36,12 +49,16 @@ class Persona {
 //crear una nueva instancia de esta clase
 const spiderman = new Persona('Peter Parker', 'Spider', 'soy tu amigo spiderman');
 const ironman = new Persona('Tony Stark', 'ironman', 'yo soy ironman');
-console.log(spiderman);
-console.log(ironman);
+//console.log(spiderman); //lo lleve abajo
+//console.log(ironman);
 //............................................
 //este es parte del metodo
 spiderman.quienSoy();
-ironman.quienSoy();
+//ironman.quienSoy();
 //..........................................
 spiderman.miFrase();
-ironman.miFrase();
+//ironman.miFrase();
+//................................... SET
+spiderman.setComidaFavorita = 'El pie  de cereza de la tia May';
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman)
