b818de246226c1ad7729839a8d7ca4a629a6114b
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-24 19:26:56 -0400

    Manejo de errores en los Callbacks

--- Course_JavaScript_GustavoBautista/06-advanced/src/concepts/02-callbacks.js
+++ Course_JavaScript_GustavoBautista/06-advanced/src/concepts/02-callbacks.js
@@ -11,8 +11,14 @@ export const callbacksComponent = (element) => {
 
 
     const id = '5d86371f2343e37870b91ef1';
-    findHero(id, (heroHulk) => { // se va mandar el id como argumento
-        element.innerHTML = heroHulk.name;
+    findHero(id, (error, hero) => { // se va mandar el id como argumento
+        // element.innerHTML = hero?.name || 'No hay heroe';
+
+        if (error){
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero.name;
     }); 
     //el valor de esa variable es esta funcion en cual cae en el callback de abajo
 
@@ -22,11 +28,16 @@ export const callbacksComponent = (element) => {
 /**
  * 
  * @param {String} id 
- * @param {(hero: Object)=> void} callback 
+ * @param {(error: String|null, hero: Object)=> void} callback 
  */
 const findHero = (id, callback) => {// se va recibi el id y el callback
     const hero = heroes.find( hero => hero.id === id); //se puede tener un trabajo asincrono
 
-    callback(hero); // se esta llamando el callback con el argumento de hero
+    if (!hero){
+        callback(`Hero with id ${id} not found`);
+        return; // regresa undefined
+    }
+
+    callback( null, hero); // se esta llamando el callback con el argumento de hero
 
 }
