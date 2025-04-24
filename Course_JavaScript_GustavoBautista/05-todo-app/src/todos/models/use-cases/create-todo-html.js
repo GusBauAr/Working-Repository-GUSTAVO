dc4c6c16b1ef173d787846eb80e1f748a96c948a
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-24 05:55:14 -0400

    Validaciones y optimizaciones

--- Course_JavaScript_GustavoBautista/05-todo-app/src/todos/models/use-cases/create-todo-html.js
+++ Course_JavaScript_GustavoBautista/05-todo-app/src/todos/models/use-cases/create-todo-html.js
@@ -7,12 +7,27 @@ import { Todo } from "../todo.model";
  * @param {Todo} todo 
  */
 export const createTodoHTML = ( todo ) => {
-    if (!todo) throw new Error('A todo object is requireed');
+    if (!todo) throw new Error('A todo object is required');
+    
+    const {done, description, id} = todo;
 
-    const html = `<h1>${todo.description}</h1>`;
+    const html = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${done ? 'checked': ''}>
+            <label>${description}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    `;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', id);
+
+    if (todo.done)
+        liElement.classList.add('completed');
+
+
 
     return liElement;
     
--- Course_JavaScript_GustavoBautista/05-todo-app/src/todos/models/use-cases/render.todos.js
+++ Course_JavaScript_GustavoBautista/05-todo-app/src/todos/models/use-cases/render.todos.js
@@ -1,6 +1,9 @@
 import { Todo } from '../todo.model';
 import { createTodoHTML } from './'; //se importa de los casos de uso por eso no hay necedidad de colocar el nombre de la carpeta por que busca ahi mismo
 
+
+let element;
+
 /**
  * 
  * @param {String} elementId 
@@ -10,8 +13,13 @@ import { createTodoHTML } from './'; //se importa de los casos de uso por eso no
 export const renderTodos = (elementId, todos = []) => {
     //aqui va generar los todos
 
-    //TODO: referencia
-    const element = document.querySelector (elementId);
+    
+    if (!element)
+        element = document.querySelector(elementId);
+
+    if (!element) throw new Error (`Element ${elementId} not found`);
+
+    element.innerHTML = '';
 
     todos.forEach( todo => {
         element.append(createTodoHTML(todo))
