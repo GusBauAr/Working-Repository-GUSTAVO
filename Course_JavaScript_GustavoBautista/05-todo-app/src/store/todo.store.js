65cca2c08d850bc2248524fdd4322ba0ae835099
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-24 04:23:19 -0400

    Implementar metodos del store

--- Course_JavaScript_GustavoBautista/05-todo-app/src/store/todo.store.js
+++ Course_JavaScript_GustavoBautista/05-todo-app/src/store/todo.store.js
@@ -3,7 +3,7 @@ import{Todo} from '../todos/models/todo.model';
 
 const Filters = {
     All: 'all',
-    Completed: 'complete',
+    Completed: 'completed',
     Pending: 'pending'
 }
 
@@ -12,7 +12,7 @@ const Filters = {
 //tods todos los objetos pasan por referencia
 const state = {
     todos: [ 
-        
+        //recordar que todos los objetos pasan por referencia
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
@@ -29,13 +29,32 @@ const loadStore = () => {
     throw new Error('Not implemented'); 
 }
 
+const getTodos = (filter = Filters.All ) => { //tenemos 3 tipos de filtros, all, completed y pending
+    switch(filter){
+        case Filters.All:
+            return [...state.todos]; // se puedo utilizar el operador spreed (...) y regresar un nuevo arreglo con cana uno de estos valores
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done); //con ese filter va regresar un nuevo arreglo. (todo.done esta en true)
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);// (!todo.done, esta en false se invirtio el true por false por el --> !)
+        default:
+            throw new  Error(`Option ${filter} is not  valid`);
+        
+
+    }
+}
+
 /**
  * 
  * @param {String} descripcion 
  */
 const addTodo = ( descripcion) => { //AGREGAR
-    throw new  Error('Not implemented');
+    if  (!descripcion) throw new Error('Description is required');
+
+    state.todos.push(new Todo(descripcion));
 }
+
+
 /**
  * 
  * @param {String} todoId /es un identificador
@@ -44,30 +63,44 @@ const toggleTodo = (todoId) => { //UN TOGGLE COMO UNA ACTUALIZACION
     throw new  Error('Not implemented');
 }
 
+
+
 const deleteTodo = (todoId) => {
-    throw new  Error('Not implemented');
+    state.todos = state.todos.filter(todo => todo.id !== todoId);//vamos a regresar todos los todos que no coincida con el id (todoId)
 }
+
+
 
 const deleteCompleted = (todoId) => {
-    throw new  Error('Not implemented');
+    state.todos = state.todos.filter(todo => todo.done);
 }
 
+
+/**
+ * 
+ * @param {Filters} newFilter es de tido filters 
+ */
 const setFilter = (newFilter = Filters.All) => {
-    throw new  Error('Not implemented');
+    state.filter = newFilter;
 }
 
-const getCurrentFilter = () => {
-    throw new  Error('Not implemented');
+
+const getCurrentFilter = () => { 
+    return state.filter;
 }
+
+
 
 export default { //nuestro stor tiene todos estos metodos o funciones que nos va servir a nostros para hacer la manipulacion lo que queremos en nuestro store
-    initStore,
     addTodo,
-    toggleTodo,
-    deleteTodo,
     deleteCompleted,
-    setFilter,
+    deleteTodo,
     getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 
 }
 
