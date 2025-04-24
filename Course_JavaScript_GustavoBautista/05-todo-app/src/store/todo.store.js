ad1a41fbad3b61c3859b19ca4e72e990116c4f37
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-23 21:01:27 -0400

    Metodos que ocuparemos del Store

--- Course_JavaScript_GustavoBautista/05-todo-app/src/store/todo.store.js
+++ Course_JavaScript_GustavoBautista/05-todo-app/src/store/todo.store.js
@@ -9,9 +9,10 @@ const Filters = {
 
 
 
-
+//tods todos los objetos pasan por referencia
 const state = {
-    todos: [
+    todos: [ 
+        
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
@@ -19,13 +20,54 @@ const state = {
     filter: Filters.All,
 }
 
-const initStore = () => { //la indea de este initStore va inicializar si tengo informacion en el STORE, el initStore se va llamar en el main
-    console.log(state);
+const initStore = () => { // la indea de este initStore va inicializar si tengo informacion en el STORE, el initStore se va llamar en el main
+    console.log(state); 
     console.log('InitStore 😸');
 }
 
-export default {
+const loadStore = () => {
+    throw new Error('Not implemented'); 
+}
+
+/**
+ * 
+ * @param {String} descripcion 
+ */
+const addTodo = ( descripcion) => { //AGREGAR
+    throw new  Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} todoId /es un identificador
+ */
+const toggleTodo = (todoId) => { //UN TOGGLE COMO UNA ACTUALIZACION
+    throw new  Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new  Error('Not implemented');
+}
+
+const deleteCompleted = (todoId) => {
+    throw new  Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new  Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new  Error('Not implemented');
+}
+
+export default { //nuestro stor tiene todos estos metodos o funciones que nos va servir a nostros para hacer la manipulacion lo que queremos en nuestro store
     initStore,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 
 }
 
