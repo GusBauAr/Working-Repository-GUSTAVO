240f7832e199b7ecf941ffcdddce54e855384983
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-24 19:15:28 -0400

    Callbacks

---
+++ Course_JavaScript_GustavoBautista/06-advanced/src/concepts/02-callbacks.js
@@ -0,0 +1,32 @@
+import { heroes } from "../data/heroes";
+
+//todo: que es un callback?
+//es una funcion que recibe un argumento y ese argumento es una funcion que invocan dentro de su funcion 
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const callbacksComponent = (element) => {
+
+
+    const id = '5d86371f2343e37870b91ef1';
+    findHero(id, (heroHulk) => { // se va mandar el id como argumento
+        element.innerHTML = heroHulk.name;
+    }); 
+    //el valor de esa variable es esta funcion en cual cae en el callback de abajo
+
+
+
+}
+/**
+ * 
+ * @param {String} id 
+ * @param {(hero: Object)=> void} callback 
+ */
+const findHero = (id, callback) => {// se va recibi el id y el callback
+    const hero = heroes.find( hero => hero.id === id); //se puede tener un trabajo asincrono
+
+    callback(hero); // se esta llamando el callback con el argumento de hero
+
+}
