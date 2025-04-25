6b93e1fda683a5ce94d382838e609fe554e0450f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-25 15:51:57 -0400

    BreackingBad component

---
+++ Course_JavaScript_GustavoBautista/07-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,13 @@
+
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export  const BreackingbadApp = (element) => {
+
+    document.querySelector('#app-title').innerHTML = 'BreakingBad App';
+    element.innerHTML = 'Loading';
+    
+}
