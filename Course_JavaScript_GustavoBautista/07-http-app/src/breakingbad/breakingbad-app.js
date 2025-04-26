009bbf6348702e0d560784f53e527491cc5c0455
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-26 07:49:39 -0400

    FetchAPI - peticion http get

--- Course_JavaScript_GustavoBautista/07-http-app/src/breakingbad/breakingbad-app.js
+++ Course_JavaScript_GustavoBautista/07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,13 +1,24 @@
 
 
+/**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() =>{
+    const  res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    console.log(data[0]);
+    return data[0];
+
+}
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export  const BreackingbadApp = (element) => {
+export  const BreackingbadApp =  async(element) => {
 
     document.querySelector('#app-title').innerHTML = 'BreakingBad App';
     element.innerHTML = 'Loading';
-    
+    const quote = await fetchQuote();
+    element.innerHTML = 'tenemos data';
 }
