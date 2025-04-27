e5ce57ee472e476eb9b5cdedbe583f064671ff53
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-27 16:55:32 -0400

    Casos de uso - Crear Usuario

---
+++ Course_JavaScript_GustavoBautista/08-http-app CRUD/src/users/use-cases/save-user.js
@@ -0,0 +1,37 @@
+import {User} from '../models/user';
+
+
+
+/**
+ * 
+ * @param {Like<User>} userLike 
+ */
+export const saveUser = async(userLike) => {
+
+    const user = new  User (userLike);
+ 
+    if (user.id){
+        throw 'No implementada la actualizacion';
+        return;
+    }
+    const updateUser = await createUser (user);
+    return updateUser;
+}
+
+/**
+ * @param {Like<User>} user
+ */
+const  createUser = async(user) => {
+    const url = `${import.meta.env.VITE_BASE_URL}/users`;
+    const res = await fetch(url, {
+        method: 'POST',
+        body:JSON.stringify(user),
+        headers:{
+            'Content-type': 'application/json'
+        }
+    });
+
+    const newUser = await res.json();   
+    console.log({ newUser });
+    return newUser;
+}
