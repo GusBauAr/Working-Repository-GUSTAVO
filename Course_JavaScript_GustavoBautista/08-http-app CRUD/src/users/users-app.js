5400392ace0c662e095b25e279c3b3d651d1472f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-27 12:32:38 -0400

    Botones- next, prev y pagina actual

--- Course_JavaScript_GustavoBautista/08-http-app CRUD/src/users/users-app.js
+++ Course_JavaScript_GustavoBautista/08-http-app CRUD/src/users/users-app.js
@@ -1,3 +1,4 @@
+import { renderButtons } from './presentation/render-buttons/render-buttons';
 import { renderTable } from './presentation/render-table/render-table';
 import usersStore from './store/users-store';
 
@@ -15,4 +16,5 @@ export const UsersApp = async( element ) => {
     // console.log( usersStore.getUsers());
 
     renderTable(element);
+    renderButtons(element);
 }
