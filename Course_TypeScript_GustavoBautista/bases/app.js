43d05b1bd4ecfcd2293e036da6f4513d1f8e241d
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-28 12:45:42 -0400

    Modo observador - watch mode

--- Course_TypeScript_GustavoBautista/bases/app.js
+++ Course_TypeScript_GustavoBautista/bases/app.js
@@ -1,3 +1,8 @@
 "use strict";
-const smg = 'hola mundo';
-console.log(smg);
+const smg = 'hola mundooooo';
+const hero = {
+    name: 'Gustavo Bautista',
+    age: 2000,
+};
+hero.age = 50;
+console.log(hero.age + 1);
--- Course_TypeScript_GustavoBautista/bases/app.ts
+++ Course_TypeScript_GustavoBautista/bases/app.ts
@@ -1,6 +1,12 @@
 
 
 
-const smg: string = 'hola mundo';
+const smg: string = 'hola mundooooo';
 
-console.log(smg);
+const hero = {
+    name: 'Gustavo Bautista',
+    age: 2000,
+}
+hero.age = 50
+
+console.log(hero.age + 1);
