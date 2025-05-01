e73ac7c3092ec24a7586aa40f26f166dc4d1b5d2
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-01 05:59:16 -0400

    TAREA3

---
+++ Course_TypeScript_GustavoBautista/bases/objetos/tarea3/app 3.ts
@@ -0,0 +1,78 @@
+
+type auto ={
+    
+  carroceria: string;
+  modelo: string;
+  antibalas: boolean;
+  pasajeros: number;
+  disparar?:()=> void ;
+}
+// Objetos
+const batimovil: auto= {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4
+};
+
+const bumblebee: auto = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
+
+
+// Villanos debe de ser un arreglo de objetos personalizados
+
+type losVillanos = {
+  nombre: string;
+  edad: number;
+  mutante: boolean;
+}
+
+const villanos: losVillanos[]= [{ 
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: 50,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+
+type  charles = {
+  poder: string;
+  estatura: number;
+}
+
+const charles: charles = {
+  poder:"psiquico",
+  estatura: 1.78
+};
+
+type Apocalipsis = {
+  lider: boolean;
+  miembros: string[];
+}
+
+const apocalipsis: Apocalipsis = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique: charles | Apocalipsis;
+
+mystique = charles;
+mystique = apocalipsis;
