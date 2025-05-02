cad126f82b6f23a19ee645677b8e1b6c952f8a20
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-02 10:38:08 -0400

    Forma corta de asignar propiedades

--- Course_TypeScript_GustavoBautista/bases/classes/basica.ts
+++ Course_TypeScript_GustavoBautista/bases/classes/basica.ts
@@ -29,6 +29,32 @@
     console.log(antman);//cuando colocamos punto vemos que nos muestra solo los publicos
                         //pero no me muestra el static..????, si queremos tener acceso al estatico tenemos que hacer:
     // console.log(Avenger.avgAge )//Ahora nos muestra y el resultado en consola es 35
+//.............................................................................................................................
+//!                         FORMA CORTA DE ASIGNARPROPIEDADES
+
+    class Avenger2 {
+            
+        
+        // private name : string;
+        // public team: string;
+        // public realName?: string;
+        static avgAge:number = 35;// la propiedad estatica si se puede dejar ahi
+
+
+        //esta es una sintaxis bien comun que se ve afuera colocar private, public, lo que esta declarada arriba ya no se necesita
+        //el constructor no solo establece la propiedad name si no que tambien la crea y la establece de forma privada
+        constructor(private name: string, public team:string, public realName?: string){
+            
+            
+
+        }
+    }
+    const antman2 : Avenger2 =new Avenger2('Antman','Capital');
+    console.log(antman2);
+    //si queremos ver el avgAge
+    console.log(Avenger2.avgAge);//35
+
+
 
     
 
--- Course_TypeScript_GustavoBautista/bases/main.js
+++ Course_TypeScript_GustavoBautista/bases/main.js
@@ -11,5 +11,17 @@
     }());
     var antman = new Avenger('Antman', 'Capital');
     console.log(antman);
+    var Avenger2 = (function () {
+        function Avenger2(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+        Avenger2.avgAge = 35;
+        return Avenger2;
+    }());
+    var antman2 = new Avenger2('Antman', 'Capital');
+    console.log(antman2);
+    console.log(Avenger2.avgAge);
 })();
 //# sourceMappingURL=main.js.map
