33e6ba279876460782bbb076fa860dc4f7a42e6f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-04 16:47:47 -0400

    TAREA-resolver error en typeScript

---
+++ Course_TypeScript_GustavoBautista/ts-node TAREA/src/data/powers.ts
@@ -0,0 +1,27 @@
+
+export  interface Power{ 
+    id:number;
+    description:string;
+}
+
+
+const powers: Power[] =[
+    {
+        //aqui agregamos el id y description
+        id: 1,
+        description: 'money',
+    },
+    {
+        //aqui agregamos el id y description
+        id: 2,
+        description: 'Drugs',
+    },
+    
+];
+
+export  default powers; //export defauld
+
+
+
+
+
