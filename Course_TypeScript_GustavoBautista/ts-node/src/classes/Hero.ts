adb6767e044af768d45724b4f2cfef94c17ad781
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-04 15:44:19 -0400

    Imports y exports

---
+++ Course_TypeScript_GustavoBautista/ts-node/src/classes/Hero.ts
@@ -0,0 +1,13 @@
+//creamo suna clase
+//para exportar mi clase a otros archivos hay que colocar antes de class (export)
+export class Hero{
+    //creamos nuestro constructor
+    constructor(
+        public name:string,
+        public powerId: number,
+        public age:number,
+        //necesito todos estos publics para crear instancias de mi clase en este caso Hero
+        //ahora nos vamos al intex.ts
+
+    ){}
+}
--- Course_TypeScript_GustavoBautista/ts-node/src/index.ts
+++ Course_TypeScript_GustavoBautista/ts-node/src/index.ts
@@ -1,11 +1,13 @@
-
-
-console.log('Hola Mundo de GUSTAVO!');
+import { Hero } from "./classes/Hero"; //las llaves ---> {} significa una desestructuracion de la carpeta from "./classes/Hero";
 
 //la idea del proyecto es que nos ayude a nosotros  a comprender  como podemos seguir trabajndo en typeScript pero ya ahora manejando modulos y usando importancionesy otro monton de cracteristicas que gracias al manejador de modulos lo tenemos 
 
 
+//para instanciar mi clase seria
 
+const ironman = new Hero('Ironman',1, 55 );
+
+console.log(ironman);
 
 
 
