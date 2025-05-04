64781a37031ae52f4918cad3e8f2d8a2bb830f10
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-04 14:03:24 -0400

    Inicio de proyecto - Modulos y Webpack

---
+++ Course_TypeScript_GustavoBautista/ts-node/src/index.ts
@@ -0,0 +1,13 @@
+
+
+console.log('Hola Mundo de GUSTAVO!');
+
+//la idea del proyecto es que nos ayude a nosotros  a comprender  como podemos seguir trabajndo en typeScript pero ya ahora manejando modulos y usando importancionesy otro monton de cracteristicas que gracias al manejador de modulos lo tenemos 
+
+
+
+
+
+
+
+
