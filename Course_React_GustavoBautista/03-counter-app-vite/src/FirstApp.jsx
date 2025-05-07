a1008345074fbf9624dc0fde5007c59fa66c921e
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-07 11:14:21 -0400

    Retornar elementos en el Componente - Fragment (modified)

--- Course_React_GustavoBautista/03-counter-app-vite/src/FirstApp.jsx
+++ Course_React_GustavoBautista/03-counter-app-vite/src/FirstApp.jsx
@@ -4,6 +4,7 @@ export const FirstApp =() =>{
         //esto es un sinomino de un fracmento, es un agrupador de otros elentos htmlen este caso tenemos neustro nodo padre (<></>)
         <> 
         <h1>GUSTAVO BAUTISTA ARCE-OZZY</h1>
+        <p>HOLA SOY OZZY</p>
         </>
 
     );
