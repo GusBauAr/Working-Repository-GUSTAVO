4e3d122ff84d0c5d000c8bf6d1fa34851fe927e6
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-07 14:33:18 -0400

    Impresión de variables en el HTML

--- Course_React_GustavoBautista/03-counter-app-vite/src/FirstApp.jsx
+++ Course_React_GustavoBautista/03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,18 @@
+
+
+const getResult =(a, b) =>{
+    return a + b;
+}
+
 export const FirstApp =() =>{
+
+    const newMessage = 'Gustavo'; //el newMessage esta dentro functional component
 
     return(
         //esto es un sinomino de un fracmento, es un agrupador de otros elentos htmlen este caso tenemos neustro nodo padre (<></>)
+        //y colocamos el newMessage en el H1 para que salga Gustavo
         <> 
-        <h1>GUSTAVO BAUTISTA ARCE-OZZY</h1>
+        <h1>{ getResult(1 , 5) }</h1> 
         <p>HOLA SOY OZZY</p>
         </>
 
