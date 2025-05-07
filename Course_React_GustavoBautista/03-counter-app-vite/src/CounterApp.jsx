45be5fa98a6e15825fd5cd0f6dbbab9009918ed4
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-07 17:09:13 -0400

    Evento click (Eventos en general)

--- Course_React_GustavoBautista/03-counter-app-vite/src/CounterApp.jsx
+++ Course_React_GustavoBautista/03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,26 @@
 
 import PropTypes from 'prop-types';
-
 import React from 'react'
 
-export const CounterApp = ({value}) => {
+
+
+export const CounterApp = ({ value }) => {
+
+    const handleAdd  = () =>{
+        // console.log(event)
+        console.log('+1');
+        value = 1000;
+    }
+
+
   return (
     <>
         <div>CounterApp</div>
-        <h2>{value}</h2>
+        <h2> { value } </h2>
+        
+        <button onClick={handleAdd}>
+            +1
+        </button>
     </>
     
   )
