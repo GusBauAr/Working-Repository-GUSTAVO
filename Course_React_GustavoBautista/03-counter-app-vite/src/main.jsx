5a339445aa43e15eac937530691f5adc2f7d6cac
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-07 10:44:00 -0400

    Hola Mundo en React

--- Course_React_GustavoBautista/03-counter-app-vite/src/main.jsx
+++ Course_React_GustavoBautista/03-counter-app-vite/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// se necesita esta importanciones para renderizar mi aplicacion..........................................
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// ..........................................
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//creamo snuestra funcion 
+function App () {
+    return(<h1>Hola mundo GUSTAVOooooo</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
