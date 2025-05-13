c9609b9497f6f2d266e00634685456f39887e8e6
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-13 15:15:47 -0400

    GifGrid - Nuevo componente

--- Course_React_GustavoBautista/04-gif-expert-app/src/GifExpertApp.jsx
+++ Course_React_GustavoBautista/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory } from './components/addCategory';
+import { GifGrid } from './components/GifGrid';
 
 
 export const GifExpertApp = () => {
@@ -32,14 +33,15 @@ export const GifExpertApp = () => {
 
         {/* listado de gifs */}
 
-        
-        <ol>
           {
-          categories.map((category) => {
-            return<li key={category}>{category}</li>
-          })
+            categories.map((category) => (
+                <GifGrid 
+                key={category} 
+                category={category}
+                />
+            ))
           }
-        </ol>
+        
 
         {/* git item */}
     </>
---
+++ Course_React_GustavoBautista/04-gif-expert-app/src/components/GifGrid.jsx
@@ -0,0 +1,10 @@
+
+export const GifGrid = ({category}) => {
+  return (
+    //creamo sun fragmento
+    <>
+        <h3>{category}</h3>
+        
+    </>
+  )
+}
