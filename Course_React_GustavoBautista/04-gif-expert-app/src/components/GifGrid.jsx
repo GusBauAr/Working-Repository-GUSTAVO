8e72f4880a46683d558f3aaf995c15746c81330f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-14 07:18:30 -0400

    className - Clases de css

--- Course_React_GustavoBautista/04-gif-expert-app/src/components/GifGrid.jsx
+++ Course_React_GustavoBautista/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,32 +1,29 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
-
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs(category)
-    .then(newImages => setImages(newImages));
-
-
-  },[])
-
+    getGifs(category).then((newImages) => setImages(newImages));
+  }, []);
 
   return (
     //creamo sun fragmento
     <>
       <h3>{category}</h3>
-      <ol>
+      <div className="card-grid">
         {
           //mostrar los titulos de las imagenes
-          images.map(({id, title}) => (
-            <li key={id}>{title}</li>
-
+          images.map((image) => (
+            <GifItem 
+            key={image.id} 
+            { ...image }
+            />
           ))
         }
-      </ol>
-   
+      </div>
     </>
   );
 };
