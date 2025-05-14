eeabac4fedecdf7092e35d24faa3d68940f5ed0d
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-14 06:01:10 -0400

    Demostración de producción rápido

--- Course_React_GustavoBautista/04-gif-expert-app/src/helpers/getGifs.js
+++ Course_React_GustavoBautista/04-gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 //creamos una funcion
 export const getGifs = async(category) =>{
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=dZEAZ9gDmKsDMe2dZAHEkom6Lc7KZcxd&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=dZEAZ9gDmKsDMe2dZAHEkom6Lc7KZcxd&q=${category}&limit=10`;
     //peticion http
     const resp = await fetch (url);
     //desestructuramos la data
@@ -13,7 +13,7 @@ export const getGifs = async(category) =>{
       title: img.title,
       url: img.images.downsized_medium.url
   }));
-  console.log(gifs);
+  
   return gifs;
 
 }
