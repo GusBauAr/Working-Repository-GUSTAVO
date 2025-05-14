46d960d6ce7d11e57d715956617e42a3ca69e829
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-14 05:45:37 -0400

    useEffect

--- Course_React_GustavoBautista/04-gif-expert-app/src/components/GifGrid.jsx
+++ Course_React_GustavoBautista/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,18 +1,21 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
+  const [counter, setCounter] = useState(10);
 
+  useEffect(() => {
     getGifs(category);
+
+  },[])
+
 
   return (
     //creamo sun fragmento
     <>
-        <h3>{category}</h3>
-        
+      <h3>{category}</h3>
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
-  )
-}
-
-
-
-
+  );
+};
