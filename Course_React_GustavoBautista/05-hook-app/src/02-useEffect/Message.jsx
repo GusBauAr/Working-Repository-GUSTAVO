eb3a12594615a93dcb07554ee87af8e5643ea943
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-16 11:50:50 -0400

    useEffect - Precauciones

--- Course_React_GustavoBautista/05-hook-app/src/02-useEffect/Message.jsx
+++ Course_React_GustavoBautista/05-hook-app/src/02-useEffect/Message.jsx
@@ -1,17 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
-  useEffect(() => {
-    console.log("message montado");
 
+  const [coords, setCoords] = useState({x:0, y:0});
+
+
+
+  useEffect(() => {
+
+    const onMouseMove = ({x, y}) => {
+      // const coords = {x, y};
+      setCoords({x, y});
+    }
+
+
+    window.addEventListener('mousemove', onMouseMove);
     return () => {
-      console.log("message desmontado");
+      window.removeEventListener('mousemove', onMouseMove);
     };
   }, []);
 
   return (
+    
     <>
       <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)} 
     </>
   );
 };
