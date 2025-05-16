50295bfa80e8edb90d54e8d718da9d708c442185
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-15 20:49:58 -0400

    Exponer métodos del Hook

--- Course_React_GustavoBautista/05-hook-app/src/hooks/useCounter.js
+++ Course_React_GustavoBautista/05-hook-app/src/hooks/useCounter.js
@@ -5,9 +5,21 @@ export const useCounter = (initialValue = 10) => {
 
     const [counter, setCounter] = useState(initialValue)
 
+    const increment = () =>{
+        setCounter(counter + 1);
+    }
+    const decrement = () =>{
+        setCounter(counter - 1);
+    }
+    const reset = () =>{
+        setCounter(initialValue);
+    }
 
     return{
-        counter: counter
+        counter: counter,
+        increment: increment,
+        decrement:decrement,
+        reset:reset,
 
 
     }
