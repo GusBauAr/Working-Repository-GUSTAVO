8f0d1f2f240da42ee55c51f1780739784e82c61b
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-18 22:25:49 -0400

    Tarea Memorize

--- Course_React_GustavoBautista/05-hook-app/src/main.jsx
+++ Course_React_GustavoBautista/05-hook-app/src/main.jsx
@@ -10,11 +10,12 @@ import './index.css'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
-import { CallBackHook } from './06-memos/CallBackHook'
+// import { CallBackHook } from './06-memos/CallBackHook'
+import { Padre } from './07-tarea-memo/Padre'
 // import { HooksApp } from './HooksApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <CallBackHook/>
+    <Padre/>
   // </StrictMode>,
 )
