0589cdb1d8846c4e1fc6ff0d74a022bfb5efb30b
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-18 22:04:22 -0400

    useCallback

--- Course_React_GustavoBautista/05-hook-app/src/main.jsx
+++ Course_React_GustavoBautista/05-hook-app/src/main.jsx
@@ -9,11 +9,12 @@ import './index.css'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallBackHook } from './06-memos/CallBackHook'
 // import { HooksApp } from './HooksApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <MemoHook/>
+    <CallBackHook/>
   // </StrictMode>,
 )
