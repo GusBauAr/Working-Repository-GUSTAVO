64089c6b52121960fe2de59d1c204dfe441be465
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-19 11:29:46 -0400

    Creando el cascarón de la lista de TODOs

--- Course_React_GustavoBautista/06-hook-app-useReducer/src/08-useReducer/TodoApp.jsx
+++ Course_React_GustavoBautista/06-hook-app-useReducer/src/08-useReducer/TodoApp.jsx
@@ -1,37 +1,54 @@
-import { useReducer } from "react"
-import { todoReducer } from "./todoReducer"
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
-const initialState =[
-    {
-        id: new Date().getTime(),
-        description:'recolectar la piedra del alam',
-        done:false,
-    },
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: "recolectar la piedra del alam",
+    done: false,
+  },
 
-
-    {
-        id: new Date().getTime() * 3,
-        description:'recolectar la piedra del alam',
-        done:false,
-    },
-]
+  {
+    id: new Date().getTime() * 3,
+    description: "recolectar la piedra del alam",
+    done: false,
+  },
+];
 
 export const TodoApp = () => {
-
-    const [ state, dispatch ] = useReducer(todoReducer, initialState)
-
-
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <>
-        <h1>TodoApp</h1>
-        <hr/>
+      <h1>
+        TodoApp 10, <small>pendientes: 2</small>
+      </h1>
+      <hr/>
 
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger">Borrar</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar Todo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="quue hay que hacer?"
+              className="from-control"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
