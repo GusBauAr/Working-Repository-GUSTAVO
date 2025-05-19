6972344389261cccd66490dd5635eec0e7f84e0e
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-18 21:19:58 -0400

    Memo - Método de React

--- Course_React_GustavoBautista/05-hook-app/src/main.jsx
+++ Course_React_GustavoBautista/05-hook-app/src/main.jsx
@@ -7,11 +7,12 @@ import './index.css'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 // import { HooksApp } from './HooksApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Layout/>
+    <Memorize/>
   // </StrictMode>,
 )
