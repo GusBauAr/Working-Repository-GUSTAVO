be0e89979caebfeed72fb998c19977589ecafd66
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-19 10:41:04 -0400

    Idea general de un reducer - Vía código

--- Course_React_GustavoBautista/06-hook-app-useReducer/src/08-useReducer/intro-reducer.js
+++ Course_React_GustavoBautista/06-hook-app-useReducer/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,44 @@
-console.log('Hola mundo gus')
+
+
+//estado inicial
+const initialState =[{
+    id:1,
+    todo: ' recolectar la piedra del alma',
+    done: false,
+}]
+
+
+//useReducer---> una funcion // el estado inicial mandamos a nuestro reducer y tenemos una accion para regresar un nuevo estado
+const todoReducer = ( state = initialState, action = {} ) => {
+
+    if (action.type === '[TODO] add todo'){
+        return[...state, action.payload];
+    }
+
+    //siempre tiene que regresar un estado
+    return state;
+
+}
+
+let todos = todoReducer();
+
+//para hacer una modificacion a nuestro reducer, hayq ue darle una accion y esa accion le dice que modificarce
+//creamos un todo
+const newTodo ={
+    id:2,
+    todo:'recolectar la piedra del poder',
+    done:false,
+}
+
+//este es nuestra accion
+const addTodoAction = {
+    type:'[TODO] add todo',
+    payload: newTodo,
+}
+
+todos = todoReducer(todos, addTodoAction );
+
+console.log({state: todos});
+
+
+
