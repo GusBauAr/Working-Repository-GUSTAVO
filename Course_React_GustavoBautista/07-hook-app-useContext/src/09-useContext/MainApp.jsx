ba9577d629ce981febaf407f6db9addea94a8324
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-21 11:25:32 -0400

    Link

--- Course_React_GustavoBautista/07-hook-app-useContext/src/09-useContext/MainApp.jsx
+++ Course_React_GustavoBautista/07-hook-app-useContext/src/09-useContext/MainApp.jsx
@@ -1,13 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes, Link } from "react-router-dom"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
 import { AboutPage } from "./AboutPage"
+import { NavBar } from "./NavBar"
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link> */}
+
+        <NavBar/>
         <hr/>
+
 
         <Routes>
             <Route path="/" element={ <HomePage/> }/>
