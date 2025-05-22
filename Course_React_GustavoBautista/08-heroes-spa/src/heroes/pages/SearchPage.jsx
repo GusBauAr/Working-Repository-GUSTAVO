ed4c48f251fe9c333e1c870ea2ed1d1d49598a2e
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-22 12:12:26 -0400

    Mostrar mensajes condicionales

--- Course_React_GustavoBautista/08-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ Course_React_GustavoBautista/08-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -12,7 +12,10 @@ export const SearchPage = () => {
 
   
   const { q = ''}  = queryString.parse( location.search );
-  const heroes = getHeroesByName(q);
+  const heroes = getHeroesByName(q); //heroes que ya estan filtrados
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
 
 
   
@@ -22,9 +25,10 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
       event.preventDefault();
-      if( searchText.trim().length <= 1) return;
 
-      navigate(`?q=${ searchText }`)
+      // if( searchText.trim().length <= 1) return;
+
+      navigate(`?q=${ searchText }`);
       
   }
   return (
@@ -52,8 +56,21 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Result</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+
+          {/* { //ternario
+            ? <div className="alert alert-primary">Search a hero</div>
+            : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+          } */} 
+          
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{display: showSearch? '' : 'none' }}>
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger animate__animated animate__fadeIn" style={{display: showError ? '' : 'none' }}>
+            No hero with <b>{ q }</b>  
+          </div>
+
+          
           {/* <HeroCard/> */}
           {
             heroes.map(hero =>(
