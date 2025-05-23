afa49da386d578e89350726b228d8c107873adee
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-23 16:49:28 -0400

    Configuración de Rutas principales y secundarias

--- Course_React_GustavoBautista/10-journal-app/src/JournalApp.jsx
+++ Course_React_GustavoBautista/10-journal-app/src/JournalApp.jsx
@@ -1,9 +1,10 @@
+import { AppRouter } from "./router/AppRouter"
 
 
 export const JournalApp = () => {
   return (
     <>
-        <h1>Hola Mundo de gustavo</h1>
+        <AppRouter/>
     </>
   )
 }
