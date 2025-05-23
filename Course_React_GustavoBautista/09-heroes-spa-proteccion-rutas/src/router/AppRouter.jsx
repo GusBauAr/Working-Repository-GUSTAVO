6786c33faab9c462ab7d5190e3807354c6868a35
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-22 22:05:10 -0400

    Rutas privadas

--- Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/router/AppRouter.jsx
+++ Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { LoginPage } from "../auth/pages/LoginPage";
 import { Navbar } from "../ui";
 import { HeroesRoutes } from "../heroes";
 import { Route, Routes } from "react-router-dom";
+import { PrivateRouter } from "./PrivateRouter";
 
 export const AppRouter = () => {
   return (
@@ -11,7 +12,14 @@ export const AppRouter = () => {
       <Routes>
         
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+
+        <Route path="/*" element={
+          <PrivateRouter>
+            <HeroesRoutes/>
+          </PrivateRouter>
+        }/>
+
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
         
       </Routes>
     </>
