93f754a180cb70d99c4009d36dea360c31e6aad0
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-22 20:47:27 -0400

    Logout del usuario

--- Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/auth/context/AuthProvider.jsx
+++ Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/auth/context/AuthProvider.jsx
@@ -40,11 +40,20 @@ export const AuthProvider = ({children}) => {
       dispatch(action)
     }
 
+    const logout = () =>{
+      localStorage.removeItem('user');
+      const  action = {type: types.logout}
+      dispatch(action);
+    }
+
 
   return (
    <AuthContext.Provider value={{
     ...authState,
-    login: login
+
+    //metodos
+    login: login,
+    logout: logout,
    }}>
     {children}
    </AuthContext.Provider>
