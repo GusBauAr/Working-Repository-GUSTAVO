f16db52ce72a8989a504db88289c65b5bc907128
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-22 22:34:36 -0400

    Rutas públicas

--- Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/router/AppRouter.jsx
+++ Course_React_GustavoBautista/09-heroes-spa-proteccion-rutas/src/router/AppRouter.jsx
@@ -4,14 +4,18 @@ import { Navbar } from "../ui";
 import { HeroesRoutes } from "../heroes";
 import { Route, Routes } from "react-router-dom";
 import { PrivateRouter } from "./PrivateRouter";
+import { PublicRouter } from "./PublicRouter";
 
 export const AppRouter = () => {
   return (
     <>
      
       <Routes>
-        
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={
+          <PublicRouter>
+            <LoginPage/>
+          </PublicRouter>
+        }/>
 
         <Route path="/*" element={
           <PrivateRouter>
@@ -19,6 +23,7 @@ export const AppRouter = () => {
           </PrivateRouter>
         }/>
 
+        {/* <Route path="/login" element={<LoginPage />} /> */}
         {/* <Route path="/*" element={<HeroesRoutes />} /> */}
         
       </Routes>
