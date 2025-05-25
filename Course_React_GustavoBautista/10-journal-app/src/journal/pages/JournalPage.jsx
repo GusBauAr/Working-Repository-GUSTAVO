67c5f327a03fa8d8b5591e69b2e684821e8e3d9c
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-24 22:58:24 -0400

    NothingSelectedView - No hay nada seleccionado

--- Course_React_GustavoBautista/10-journal-app/src/journal/pages/JournalPage.jsx
+++ Course_React_GustavoBautista/10-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,16 +1,18 @@
 
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         GitHub's “Hello World” tutorial teaches you essentials, where you create your own repository and learn GitHub's pull request workflow for creating and reviewing code.
-      </Typography>
+      </Typography> */}
 
       {/* NothinSelected */}
+      <NothingSelectedView/>
       {/* NoteView */}
     </JournalLayout>
   )
