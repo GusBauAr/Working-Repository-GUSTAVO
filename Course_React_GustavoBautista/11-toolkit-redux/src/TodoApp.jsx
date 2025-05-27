21f5e5e1a1898d1d65af565cf92e5841ca3bce4c
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-27 10:01:54 -0400

    RTK Query

---
+++ Course_React_GustavoBautista/11-toolkit-redux/src/TodoApp.jsx
@@ -0,0 +1,15 @@
+
+
+export const TodoApp = () => {
+  return (
+   <>
+    <h1> todos - RTk query</h1>
+    <hr />
+    <h4>isLoading</h4>
+
+    <pre></pre>
+
+    <button>Next Todo</button>
+   </>
+  )
+}
