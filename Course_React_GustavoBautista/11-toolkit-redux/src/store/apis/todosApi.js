09ee2d1f9e8e0c286708e9dd29a3b20478bf19e3
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-27 10:02:21 -0400

    RTK Query - update

--- Course_React_GustavoBautista/11-toolkit-redux/src/store/apis/todosApi.js
+++ Course_React_GustavoBautista/11-toolkit-redux/src/store/apis/todosApi.js
@@ -1,3 +1,17 @@
-import { createApi } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-createApi
+export const todosApi = createApi({
+    reducerPath:'todos',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'http://jsonplaceholder.typicode.com'
+    }),
+    endpoints: (builder) => ({
+        getTodos:builder.query({
+            query: () => '/todos' //llamamos la url y concatenamos con el /todos
+        })
+    })
+
+})
+
+export const {useGetTodosQuery} = todosApi;//aqui nos permite crear custom hooks
+//este custom hook es el que va tener todoa la informacion necesaria para saber cuando tenemos errores, cuando se carga
