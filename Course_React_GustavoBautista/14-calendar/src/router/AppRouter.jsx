e6796ef1b0d0f3ac03a14abecf255ae01e80858f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-09 12:50:05 -0400

    LoginScreen y Navbar

--- Course_React_GustavoBautista/14-calendar/src/router/AppRouter.jsx
+++ Course_React_GustavoBautista/14-calendar/src/router/AppRouter.jsx
@@ -4,12 +4,12 @@ import { CalendarPage } from "../calendar/pages/CalendarPage";
 
 
 export const AppRouter = () => {
-  const authStatus = 'not-authenticated';
+  const authStatus = 'authenticated';
 
   return (
     <Routes>
         {
-            (authStatus === 'authenticated')
+            (authStatus === 'not-authenticated')
             ? <Route path="/auth/*" element={<LoginPage/>}/>
             : <Route path="/*" element={<CalendarPage/>}/>
             
