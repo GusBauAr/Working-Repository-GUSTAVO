55a2a11c19a9e8145557b5506308f293a711cc6a
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-08 20:57:38 -0400

    Mostrar las notas en el menú lateral

--- Course_React_GustavoBautista/13-journal-app-redux-CRUD/src/journal/components/SideBar.jsx
+++ Course_React_GustavoBautista/13-journal-app-redux-CRUD/src/journal/components/SideBar.jsx
@@ -1,11 +1,13 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SiderBarItem } from "./SiderBarItem";
 
 
 export const SideBar = ({drawerWidth = 240}) => {
 
-    const {displayName} = useSelector(state => state.auth)
+    const {displayName} = useSelector(state => state.auth);
+    const {notes} = useSelector(state => state.journal);
 
 
 
@@ -31,20 +33,8 @@ export const SideBar = ({drawerWidth = 240}) => {
             <Divider/>
             <List>
                 {
-                    ['enero','febrero','marzo', 'abril'].map(text => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot/>
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={ text }/>
-                                    <ListItemText secondary={'hola soy Gustavo Bautista Arce mi mascota es ozzy'}/>
-                                        
-                                </Grid>
-                            </ListItemButton>
-
-                        </ListItem>
+                    notes.map(note => (
+                        <SiderBarItem key={note.id} { ...note }/>
                     ))
                 }
             </List>
