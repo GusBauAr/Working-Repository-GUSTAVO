9485ae9e541fe8a7a68796b9122f2654a60798b6
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-09 13:07:28 -0400

    React Big Calendar

--- Course_React_GustavoBautista/14-calendar/src/calendar/pages/CalendarPage.jsx
+++ Course_React_GustavoBautista/14-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import enUS from 'date-fns/locale/en-US'
+import { addHours, format, parse, startOfWeek, getDay  } from 'date-fns';
+
 import { NavBar } from "../components/NavBar"
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title:'cimple de gustavo',
+  notes: 'hay que comprar torta',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: ' #fafafa',
+  user:{
+    _id:'123',
+    name:'gustavo'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar/>
+
+      <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+      />
     </>
   )
 }
