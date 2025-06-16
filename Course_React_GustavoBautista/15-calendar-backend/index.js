34f402bd584b7bbe1154e2b0eb7b92dd0ce3723f
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-16 13:49:10 -0400

    Configurando Express

--- Course_React_GustavoBautista/15-calendar-backend/index.js
+++ Course_React_GustavoBautista/15-calendar-backend/index.js
@@ -1,1 +1,20 @@
-console.log('hola mundo Gus')
+const express = require('express');
+
+
+//crear el servidor de express
+
+const app = express();
+
+
+//configuracion de mi primera ruta (RUTAS)
+app.get('/', (req, res)=>{
+    res.json({
+        ok: true
+    })
+})
+
+//escuchar peticiones
+app.listen(4000, ()=> {
+    console.log(`servidor corriendo en puerto ${ 4000 }`)
+})
+//aqui mostamos un backend server
