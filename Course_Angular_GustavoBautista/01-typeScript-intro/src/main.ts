909e44df29566e326df3a114c1de4c341d12f992
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-11-12 09:44:34 -0400

    Tipos básicos y conceptos generales

--- Course_Angular_GustavoBautista/01-typeScript-intro/src/main.ts
+++ Course_Angular_GustavoBautista/01-typeScript-intro/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-
+import './topics/01-basic-types'
+//importamos iempre un modulo en este caso de import './topics/01-basic-types' este si es un modulo
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 
---
+++ Course_Angular_GustavoBautista/01-typeScript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,8 @@
+const  name:string = 'gustavo';
+let hpPoints : number | string = 95;
+
+hpPoints = 'FULL';
+
+
+
+export {};
